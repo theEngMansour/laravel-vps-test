c2d62b173806fef72df64f2f659f6b12975d5ae1
Mansour Ahmed <mansour.yemen1@gmail.com>
2025-05-11 10:02:27 +0300

    add

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Spatie\LaravelPdf\Facades\Pdf;
 
 Route::get('/', function () {
-    return view('welcome');
+    return Pdf::view('pdfs.invoice')
+        ->format('a4')
+        ->name('your-invoice.pdf');
 });
