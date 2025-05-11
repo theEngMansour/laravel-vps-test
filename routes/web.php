077e10e5a9d174e0cebf50eefb162d6280bfeab2
Mansour Ahmed <mansour.yemen1@gmail.com>
2025-05-11 10:26:36 +0300

    add

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Spatie\Browsershot\Browsershot;
 use Spatie\LaravelPdf\Facades\Pdf;
 
-Route::get('/', function () {
+Route::get('/laravel-Pdf', function () {
     return Pdf::view('pdfs.invoice')
         ->format('a4')
         ->name('your-invoice.pdf');
+});
+Route::get('/browsershot', function () {
+    Browsershot::url('https://example.com')
+        ->setChromePath('/usr/bin/google-chrome') // or '/usr/bin/chromium' on Alpine
+        ->save('example.pdf');
 });
